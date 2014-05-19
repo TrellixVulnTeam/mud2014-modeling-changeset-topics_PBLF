3a70f7032ae6e8981c95ebaad9483129886418b4
Christopher Corley <cscorley@ua.edu>
2014-05-19 17:57:08 -0500

    Add header comment

--- src/generation.py
+++ src/generation.py
@@ -1,12 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# [The "New BSD" license]
+# Copyright (c) 2014 The Board of Trustees of The University of Alabama
+# All rights reserved.
+#
+# See LICENSE for details.
+
+"""
+Code for generating the corpora.
+"""
+
+from __future__ import print_function
 from io import StringIO
 
 
-def read_fname(lyst, fname_out): 
+def read_fname(lyst, fname_out):
     with open(fname_out, 'w') as g:
         for fname in lyst:
             with open(fname, 'r') as f:
                 generate_docs(fname, f, g)
-                
+
 def generate_docs(fname, infile, outfile):
     outfile.write(fname + " en")
     for line in infile:
@@ -14,7 +28,7 @@ def generate_docs(fname, infile, outfile):
         for word in line:
             outfile.write(" ")
             outfile.write(word)
-    outfile.write('\n') 
+    outfile.write('\n')
 
 def test_generate():
     fname = "butts.txt"
