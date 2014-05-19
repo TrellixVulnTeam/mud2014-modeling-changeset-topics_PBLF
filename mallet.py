5cb4c7ca46d15291e0a74ae6e3bbd71c3b23c8c7
kkashuda <klkashuda@crimson.ua.edu>
2014-05-19 17:20:05 -0500

    takes in list of files-- reads/writes

---
+++ mallet.py
@@ -0,0 +1,37 @@
+from io import StringIO
+
+
+def read_fname(lyst, fname_out): 
+    with open(fname_out, 'w') as g:
+        for fname in lyst:
+            with open(fname, 'r') as f:
+                generate_docs(fname, f, g)
+                
+def generate_docs(fname, infile, outfile):
+    outfile.write(fname + " en")
+    for line in infile:
+        line = line.strip().split()
+        for word in line:
+            outfile.write(" ")
+            outfile.write(word)
+    outfile.write('\n') 
+
+def test_generate():
+    fname = "butts.txt"
+    expected = "butts.txt en burgers cats dogs hotdogs"
+    with StringIO("burgers\ncats\ndogs hotdogs\n") as i:
+        with StringIO() as o:
+            generate_docs(fname, i, o)
+            result = o.getvalue()
+
+    assert result == expected
+
+    i = "burgers\ncats\ndogs hotdogs\n".splitlines()
+    with StringIO() as o:
+        generate_docs(fname, i, o)
+        result = o.getvalue()
+
+    assert result == expected
+
+
+
