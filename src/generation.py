b102b0cfde261dc6409d56e4baa31b0a6cd12270
Christopher Corley <cscorley@ua.edu>
2014-05-24 13:19:29 -0500

    Set up tests

--- src/generation.py
+++ src/generation.py
@@ -14,16 +14,16 @@ Code for generating the corpora.
 from __future__ import print_function
 from io import StringIO
 
-def read_in_files(): 
-    path = '/Users/kellykashuda/REU/topic-of-change/data/ant_files.txt' 
+def read_in_files():
+    path = '/Users/kellykashuda/REU/topic-of-change/data/ant_files.txt'
     # or
-    #path = '../data/ant_files.txt' 
-    list_of_files = [] 
+    #path = '../data/ant_files.txt'
+    list_of_files = []
     with open(path) as f:
         for line in f:
             stripped = line.strip()
             stuff = '../data/' + stripped
-            list_of_files.append(stuff) 
+            list_of_files.append(stuff)
     print(list_of_files[0:3])
 
 def read_fname(lyst, fname_out):
@@ -35,28 +35,10 @@ def read_fname(lyst, fname_out):
 def generate_docs(fname, infile, outfile):
     outfile.write(fname + " en")
     for line in infile:
+        line = unicode(line)
         line = line.strip().split()
         for word in line:
-            outfile.write(" ")
+            outfile.write(u" ")
             outfile.write(word)
-    outfile.write('\n')
+    outfile.write(u'\n')
 
-def test_generate():
-    fname = "butts.txt"
-    expected = "butts.txt en burgers cats dogs hotdogs"
-    with StringIO("burgers\ncats\ndogs hotdogs\n") as i:
-        with StringIO() as o:
-            generate_docs(fname, i, o)
-            result = o.getvalue()
-
-    assert result == expected
-
-    i = "burgers\ncats\ndogs hotdogs\n".splitlines()
-    with StringIO() as o:
-        generate_docs(fname, i, o)
-        result = o.getvalue()
-
-    assert result == expected
-
-
-    
