f3bd5d52a4aca0852d03663f1016a85e4d2630ab
Christopher Corley <cscorley@ua.edu>
2014-05-25 15:14:15 -0500

    Write a gensim-specific file-corpus generator

--- src/generation.py
+++ src/generation.py
@@ -13,6 +13,38 @@ Code for generating the corpora.
 
 from __future__ import print_function
 from io import StringIO
+import os
+
+import gensim
+
+class MultiTextCorpus(gensim.corpora.TextCorpus):
+    """ Iterates over all files within `top_dir`, yielding each file as
+    a document and the fully qualified name of the file as the metadata.
+
+    """
+
+    def __init__(self, top_dir):
+        # set top_dir first so get_texts doesn't get lost during TextCorpus.__init__
+        self.top_dir = top_dir
+        super(MultiTextCorpus, self).__init__()
+
+    def get_texts(self, filter_by=lambda x: True):
+        self.length = 0
+
+        for root, dirs, files in os.walk(self.top_dir):
+            # walk all files and subdirectories, yielding the contents of each
+            for fname in filter(filter_by, files):
+                fpath = os.path.join(root, fname)
+                self.length += 1
+
+                with open(fpath) as f:
+                    document = f.read()
+
+                if self.metadata:
+                    yield gensim.utils.tokenize(document, lower=True), (fpath,)
+                else:
+                    yield gensim.utils.tokenize(document, lower=True)
+
 
 def read_in_files():
     path = '/Users/kellykashuda/REU/topic-of-change/data/ant_files.txt'
@@ -41,4 +73,5 @@ def generate_docs(fname, infile, outfile):
             outfile.write(u" ")
             outfile.write(word)
     outfile.write(u'\n')
+
 
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -14,6 +14,10 @@ if __name__ == '__main__':
 from nose.tools import *
 import unittest
 
+import os.path
+module_path = os.path.dirname(__file__) # needed because sample data files are located in the same folder
+datapath = lambda fname: os.path.join(module_path, u'test_data', fname)
+
 from io import StringIO
 from src import generation
 
@@ -38,4 +42,43 @@ class TestGeneration(unittest.TestCase):
             result = o.getvalue()
 
         self.assertEqual(result, expected)
+
+    def test_multitext_corpus(self):
+        basepath = datapath(u'multitext/')
+        corpus = generation.MultiTextCorpus(basepath)
+        corpus.metadata = True
+        docs = list(corpus)
+        self.assertEqual(len(corpus), 11) # check the corpus builds correctly
+        self.assertEqual(len(docs), 11) # the deerwester corpus always has nine documents, no matter what format
+
+        documents = [
+                ([u'human', u'machine', u'interface', u'for', u'lab', u'abc', u'computer', u'applications'], 
+                    (basepath + 'a/0.txt',)),
+                ([u'a', u'survey', u'of', u'user', u'opinion', u'of', u'computer', u'system', u'response', u'time'],
+                    (basepath + 'a/1.txt',)),
+                ([u'the', u'eps', u'user', u'interface', u'management', u'system'],
+                    (basepath + 'b/2.txt',)),
+                ([u'system', u'and', u'human', u'system', u'engineering', u'testing', u'of', u'eps'],
+                    (basepath + 'b/3.txt',)),
+                ([u'relation', u'of', u'user', u'perceived', u'response', u'time', u'to', u'error', u'measurement'],
+                    (basepath + 'c/4.txt',)),
+                ([u'the', u'generation', u'of', u'random', u'binary', u'unordered', u'trees'],
+                    (basepath + 'c/e/5.txt',)),
+                ([u'the', u'intersection', u'graph', u'of', u'paths', u'in', u'trees'],
+                    (basepath + 'c/f/6.txt',)),
+                ([u'graph', u'minors', u'iv', u'widths', u'of', u'trees', u'and', u'well', u'quasi', u'ordering'],
+                    (basepath + '7.txt',)),
+                ([u'graph', u'minors', u'a', u'survey'],
+                    (basepath + 'dos.txt',)),
+                ([u'graph', u'minors', u'a', u'survey'],
+                    (basepath + 'mac.txt',)),
+                ([u'graph', u'minors', u'a', u'survey'],
+                    (basepath + 'unix.txt',)),
+                ]
+
+        for docmeta in corpus.get_texts():
+            doc, meta = docmeta
+            doc = list(doc)
+            docmeta = doc, meta
+            self.assertIn(docmeta, documents)
 
