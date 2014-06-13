995163e66bd6f197fe1ab73971db41daca9eed92
Christopher Corley <cscorley@ua.edu>
2014-06-13 12:48:11 -0500

    Skip testing libs

--- src/cli.py
+++ src/cli.py
@@ -16,6 +16,7 @@ pass_config = click.make_pass_decorator(Config, ensure=True)
 def cli(config, verbose):
     """
     Modeling Changeset Topics
+    buttst
     """
     # Only set config items here, this function is unused otherwise.
     config.verbose = verbose
