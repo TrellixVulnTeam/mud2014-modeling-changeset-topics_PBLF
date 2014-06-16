2f2234286e88a8579f24418f43a71474704e9051
Christopher Corley <cscorley@ua.edu>
2014-06-16 04:12:18 -0500

    Add cloning option, allow base_path to be set to work from

--- src/cli.py
+++ src/cli.py
@@ -5,6 +5,7 @@ import click
 class Config:
     def __init__(self):
         self.verbose = False
+        self.base_path = '.'
         # set all possible config options here
 
 
@@ -12,15 +13,29 @@ pass_config = click.make_pass_decorator(Config, ensure=True)
 
 @click.group()
 @click.option('--verbose', is_flag=True)
+@click.option('--base_path', default='/var/opt/topic-of-change/',
+        help="Set the directory to work within")
 @pass_config
-def cli(config, verbose):
+def cli(config, verbose, base_path):
     """
     Modeling Changeset Topics
-    buttst
     """
     # Only set config items here, this function is unused otherwise.
     config.verbose = verbose
+    config.base_path = base_path
 
+
+@cli.command()
+@click.argument('project')
+@pass_config
+def clone(config, project):
+    """
+    Clones the project repository
+    """
+    if config.verbose:
+        print('We are in verbose mode.')
+
+    print('Cloning repo for: %s' % project)
 
 @cli.command()
 #@click.option('--repeat', default=1,
@@ -76,6 +91,7 @@ def run_all(context, config, project):
     Runs corpora, preprocess, model, and evaluate in one shot.
     """
     print('Doing everything for: %s' % project)
+    context.forward(clone)
     context.forward(corpora)
     context.forward(preprocess)
     context.forward(model)
