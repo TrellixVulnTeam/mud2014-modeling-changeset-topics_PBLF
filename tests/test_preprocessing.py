751bbef5ce1b5b516c2745a5fa4d8ea47e1e2e10
Christopher Corley <cscorley@ua.edu>
2014-06-16 10:17:23 -0500

    Make nosetests skip everything but our tests in tests/

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -23,7 +23,51 @@ from src.preprocessing import split, remove_stops
 module_path = os.path.dirname(__file__)
 datapath = lambda fname: os.path.join(module_path, u'test_data', fname)
 
-def test_split():
-    string = ["420yoloSwag_4evr-Ever", "batMan-and_Robin", "helloWorld---_"]
-    splitted = split(string)
-    assert splitted != ["yolo", "Swag", "evr", "Ever", "bat", "Man", "and", "robin", "hello", "World"] , "test failed"
+class PreprocessTests(unittest.TestCase):
+    def test_split(self):
+        string = ["420yoloSwag_4evr-Ever", "batMan-and_Robin", "helloWorld---_"]
+        splitted = split(string)
+        #assert splitted != ["yolo", "Swag", "evr", "Ever", "bat", "Man", "and", "robin", "hello", "World"] , "test failed"
+        expected = ["yolo", "Swag", "evr", "Ever", "bat", "Man", "and", "robin", "hello", "World"]
+        self.assertEqual(list(splitted), expected)
+
+
+    def test_split_hard(self):
+        """Split tokens into terms using the following rules:
+
+            0. All digits are discarded
+            1. A sequence beginning with an lc letter must be followed by lc letters
+            2. A sequence beginning with an uc letter can be followed by either:
+                a. One or more uc letters
+                b. One or more lc letters
+
+    """
+        cases = dict({
+                'camelCase': ('camel', 'Case'),
+                'CamelCase': ('Camel', 'Case'),
+                'camel2case': ('camel', 'case'),
+                'camel2Case': ('camel', 'Case'),
+                'word': ('word'),
+                'HTML': ('HTML'),
+                'readXML': ('read', 'XML'),
+                'XMLRead': ('XML', 'Read'),
+                'firstMIDDLELast': ('first', 'MIDDLE', 'Last'),
+                'CFile': ('C', 'File'),
+                'Word2Word34': ('Word', 'Word'),
+                'WORD123Word': ('WORD', 'Word'),
+                'c_amelCase': ('c', 'amel', 'Case'),
+                'CamelC_ase': ('Camel', 'C', 'ase'),
+                'camel2_case': ('camel', 'case'),
+                'camel_2Case': ('camel', 'Case'),
+                'word': ('word'),
+                'HTML': ('HTML'),
+                'read_XML': ('read', 'XML'),
+                'XML_Read': ('XML', 'Read'),
+                'firstM_IDDL_ELast': ('first', 'M', 'IDDL', 'E', 'Last'),
+                'the_CFile': ('the', 'C', 'File'),
+                'Word_2_Word3_4': ('Word', 'Word'),
+                'WO_RD123W_or_d': ('WO', 'RD', 'W', 'or', 'd'),
+                })
+        for term, expected in cases.items():
+            result = split(term)
+            self.assertEqual(tuple(result), expected)
