99f060e8bb706e4adac81c9c9dcf15b70a6bfbda
Christopher Corley <cscorley@ua.edu>
2014-06-16 04:15:34 -0500

    Make the project name be required for any (all) commands

--- src/cli.py
+++ src/cli.py
@@ -6,6 +6,7 @@ class Config:
     def __init__(self):
         self.verbose = False
         self.base_path = '.'
+        self.project = None
         # set all possible config options here
 
 
@@ -15,82 +16,79 @@ pass_config = click.make_pass_decorator(Config, ensure=True)
 @click.option('--verbose', is_flag=True)
 @click.option('--base_path', default='/var/opt/topic-of-change/',
         help="Set the directory to work within")
+@click.argument('project')
 @pass_config
-def cli(config, verbose, base_path):
+def cli(config, verbose, base_path, project):
     """
     Modeling Changeset Topics
     """
     # Only set config items here, this function is unused otherwise.
     config.verbose = verbose
     config.base_path = base_path
+    config.project = project
 
 
 @cli.command()
-@click.argument('project')
 @pass_config
-def clone(config, project):
+def clone(config):
     """
     Clones the project repository
     """
     if config.verbose:
         print('We are in verbose mode.')
 
-    print('Cloning repo for: %s' % project)
+    print('Cloning repo for: %s' % config.project)
 
 @cli.command()
 #@click.option('--repeat', default=1,
 #        help='How many times to greet the thing.')
 @click.argument('project')
 @pass_config
-def corpora(config, project):
+def corpora(config):
     """
     Builds the basic corpora for a project
     """
     if config.verbose:
         print('We are in verbose mode.')
 
-    print('Creating corpus for: %s' % project)
+    print('Creating corpus for: %s' % config.project)
 
 
 @cli.command()
-@click.argument('project')
 @pass_config
-def preprocess(config, project):
+def preprocess(config):
     """
     Runs the preprocessing steps on a corpus
     """
-    print('Preproccessing corpus for: %s' % project)
+    print('Preproccessing corpus for: %s' % config.project)
 
 
 @cli.command()
-@click.argument('project')
 @pass_config
-def model(config, project):
+def model(config):
     """
     Builds a model for the corpora
     """
-    print('Building topic models for: %s' % project)
+    print('Building topic models for: %s' % config.project)
 
 
 @cli.command()
-@click.argument('project')
 @pass_config
-def evaluate(config, project):
+def evaluate(config):
     """
     Evalutates the models
     """
-    print('Evalutating models for: %s' % project)
+    print('Evalutating models for: %s' % config.project)
 
 
 @cli.command()
-@click.argument('project')
 @pass_config
 @click.pass_context
-def run_all(context, config, project):
+def run_all(context, config):
     """
     Runs corpora, preprocess, model, and evaluate in one shot.
     """
-    print('Doing everything for: %s' % project)
+    print('Doing everything for: %s' % config.project)
     context.forward(clone)
     context.forward(corpora)
     context.forward(preprocess)
