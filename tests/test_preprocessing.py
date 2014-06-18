7a231f3127f3a627f117e7845bcfce11b49f64f1
kkashuda <klkashuda@crimson.ua.edu>
2014-06-18 10:15:17 -0500

    stop_words test now tests for stopwords, punctuation and numbers

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -87,7 +87,7 @@ class PreprocessTests(unittest.TestCase):
 
 
     def test_stops(self): 
-        input = [u'test', u'the']
+        input = [u'test123.', u'the']
         expected = [u'test']
         stops = [u'the']
         result = remove_stops(input, stops)
