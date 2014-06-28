5fa24e00e89f75f0145f035c63bc3cd8da42ea0b
Christopher Corley <cscorley@ua.edu>
2014-06-28 12:21:20 -0500

    Begin building corpora for issue #17. Still need some stuff

--- src/main.py
+++ src/main.py
@@ -16,16 +16,20 @@ from collections import namedtuple
 import click
 import gittle
 import dulwich
+from gensim.corpora import MalletCorpus
+
+from corpora import MultiTextCorpus, ChangesetCorpus
+
 
 class Config:
     def __init__(self):
         self.verbose = False
-        self.base_path = '.'
+        self.path = './'
         self.project = None
         self.repo = None
         # set all possible config options here
 
-def error(msg, errorno=1)
+def error(msg, errorno=1):
     print(msg, file=sys.stderr)
     sys.exit(errorno)
 
@@ -34,25 +38,25 @@ pass_config = click.make_pass_decorator(Config, ensure=True)
 
 @click.group()
 @click.option('--verbose', is_flag=True)
-@click.option('--base_path', default='/var/opt/topic-of-change/',
+@click.option('--path', default='/var/opt/topic-of-change/',
         help="Set the directory to work within")
 @click.argument('project')
 @pass_config
-def main(config, verbose, base_path, project):
+def main(config, verbose, path, project):
     """
     Topic of Change
     """
 
     # Only set config items here, this function is unused otherwise.
     config.verbose = verbose
-    config.base_path = base_path
+    config.path = path
     with open("projects.csv", 'r') as f:
         reader = csv.reader(f)
         header = next(reader)
         Project = namedtuple('Project',  ' '.join(header))
 
         # figure out which column index contains the project name
-        name_idx = header.index("short_name")
+        name_idx = header.index("name")
 
         # find the project in the csv, adding it's info to config
         for row in reader:
@@ -82,15 +86,15 @@ def clone(context, config):
     while config.repo is None and tries > 0:
         tries -= 1
         try:
-            config.repo = open_or_clone(config.base_path, config.project)
+            config.repo = open_or_clone(config.path, config.project)
         except dulwich.errors.GitProtocolError as e:
             config.repo = None
 
-    if config.repo is None
+    if config.repo is None:
         error('Could not clone repository within 10 tries :(')
 
 def open_or_clone(path, project):
-    full_path = path + '/' + project.short_name
+    full_path = path + project.name
     try:
         return gittle.Gittle(full_path)
     except dulwich.errors.NotGitRepository:
@@ -106,10 +110,25 @@ def corpora(context, config):
     """
 
     if config.repo is None:
-        # clone repo
-        context.forward(clone)
+        try:
+            config.repo = gittle.Gittle(config.path + config.project.name)
+        except dulwich.errors.NotGitRepository:
+            error('Repository not cloned yet!')
 
     print('Creating corpus for: %s' % config.project.name)
+    print('Creating file-based corpus out of source files for '
+          'release %s at commit %s' % (
+              config.project.release, config.project.commit))
+
+    file_corpus = MultiTextCorpus(config.repo, config.project.commit)
+    MalletCorpus.serialize(config.path + config.project.name, file_corpus)
+
+    print('Creating changeset-based corpus out of source files for '
+          'release %s for all commits reachable from %s' % (
+              config.project.release, config.project.commit))
+
+    changeset_corpus = ChangesetCorpus(config.repo, config.project.commit)
+    MalletCorpus.serialize(config.path + config.project.name, changeset_corpus)
 
 
 @main.command()
