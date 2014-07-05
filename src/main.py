f738fe158dd8ddf3849682b0dc2d79d48230315c
Christopher Corley <cscorley@ua.edu>
2014-07-04 23:59:24 -0500

    Make sure we don't hold out more documents than needed

--- src/main.py
+++ src/main.py
@@ -292,6 +292,7 @@ class TrainingCorpus:
         return len(self.corpus) - len(self.ids)
 
     def __iter__(self):
+        self.held_out = list()
         for doc_id, doc in enumerate(self.corpus):
             if doc_id in self.ids:
                 self.held_out.append(doc)
